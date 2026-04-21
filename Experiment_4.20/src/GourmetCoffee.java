ce329971b7825e211d5f8173562d7b5c92a4f643
XuLeo-yep <baigeia114514@outlook.com>
2026-04-21 10:09:06 +0800

    today1

--- Experiment_4.20/src/GourmetCoffee.java
+++ Experiment_4.20/src/GourmetCoffee.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class GourmetCoffee {
     private Sales sales;
 
@@ -25,8 +28,36 @@ public class GourmetCoffee {
 
     }
 
-    private int getChoice(){
+    private int getChoice() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int choice = -1;
+        boolean validInput = false;
 
+        while (!validInput){
+            System.out.println();
+            System.out.println("[0] Quit");
+            System.out.println("[1] Display sales (Plain Text)");
+            System.out.println("[2] Display sales (HTML)");
+            System.out.println("[3] Display sales (XML)");
+            System.out.print("choice> ");
+
+            String input = scanner.next();
+
+            try{
+                choice = Integer.parseInt(input);
+                if(choice >= 0 && choice<= 3){
+                    validInput = true;
+                }
+                else {
+                    System.out.println("Please enter a number between 0 and 3");
+                }
+            }catch (NumberFormatException e){
+                System.out.println("Invalid input." +
+                        "Please enter a number between 0 and 3.");
+            }
+            System.out.println();
+        }
+        return choice;
     }
 
     private void setSalesFormatter(SalesFormatter newFormatter){
